08d0c12259cb3d1ea3a0f598e0ac3de0ad06bfe3
nic-williams <89101048+nic-williams@users.noreply.github.com>
2025-10-30 23:14:21 -0400

    Update gke.tf

--- gcp/gke.tf
+++ gcp/gke.tf
@@ -38,7 +38,7 @@ resource "google_container_cluster" "primary" {
 
 # Separately Managed Node Pool
 resource "google_container_node_pool" "primary_nodes" {
-  name       = google_container_cluster.primary.name
+  name       =  "${var.project_id}-gke-node-pool" # google_container_cluster.primary.name
   location   = "${var.region}-a"  # Match the cluster zone
   cluster    = google_container_cluster.primary.name
   
