e296120273714bd3fd51b386a29258c6dfa639ab
nic-williams <89101048+nic-williams@users.noreply.github.com>
2025-10-30 23:34:50 -0400

    Update gke.tf

--- gcp/gke.tf
+++ gcp/gke.tf
@@ -42,7 +42,7 @@ resource "google_container_node_pool" "primary_nodes" {
   location   = "${var.region}-a"  # Match the cluster zone
   cluster    = google_container_cluster.primary.name
   
-  version = data.google_container_engine_versions.gke_version.release_channel_default_version["STABLE"]
+  # version = data.google_container_engine_versions.gke_version.release_channel_default_version["STABLE"]
   node_count = var.gke_num_nodes
 
   node_config {
